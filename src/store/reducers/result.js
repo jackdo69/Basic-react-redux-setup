20ea8f16bf6ed3f8cf4feb33932af7390512d0ef
jackdo69 <jackie.duc.do@gmail.com>
2020-01-10 16:52:03 +1100

    restructure the reducer

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,18 +5,20 @@ const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(el => (
+        el.id !== action.id
+    ))
+    return updateObject(state, {results: updatedArray});
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
             return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})})
         case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArr = [...state.results];
-            // newArr.results.splice(id, 1)
-            const updatedArray = state.results.filter(el => (
-                el.id !== action.id
-            ))
-            return updateObject(state, {results: updatedArray});   
+            return deleteResult(state, action);
+               
     }
     return state;
 };
